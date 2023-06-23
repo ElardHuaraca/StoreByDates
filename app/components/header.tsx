113632ca90ba0ff363e0d460df19b59e2d1aeddf
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-22 20:18:20 -0500

    Update header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -17,7 +17,8 @@ export default function Header() {
         <div className="w-full min-h-[30px] p-6 shadow-lg">
             <div className="flex">
                 {ROUTES.map(route => {
-                    return <Link href={route.path} key={route.path} className="p-3 text-lg underline underline-offset-4 decoration-2 transition hover:text-orange-400 duration-700">
+                    return <Link href={route.path} key={route.path}
+                        className="p-3 text-lg underline underline-offset-4 decoration-2 transition hover:text-orange-400 duration-500">
                         {route.title}
                     </Link>
                 })}
