d11049907bc3c3a69dc4a73016322fcc01351d37
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-23 21:20:17 -0500

    Update API now consult database controller

--- app/api/stores/route.tsx
+++ app/api/stores/route.tsx
@@ -1,13 +1,10 @@
+import { AllStores } from "@/utils/DatabaseController";
 import { NextResponse } from "next/server";
 
 
 export async function GET() {
-    return NextResponse.json([
-        { id: 1, name: 'John Doe' },
-        { id: 2, name: 'Jane Doe' },
-        { id: 3, name: 'Arya Doe' },
-        { id: 4, name: 'Joe Doe' },
-        { id: 5, name: 'Jill Doe' },
-        { id: 6, name: 'Jenny Doe' },
-    ])
+
+    const stores = await AllStores()
+
+    return NextResponse.json(stores)
 }
