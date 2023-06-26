ce119513f89cbf613ee9e08f7185137ca70d0083
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-25 22:49:01 -0500

    Update header Link to a for path '/'

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link"
 
 const ROUTES = [
     {
@@ -11,12 +11,18 @@ const ROUTES = [
     }
 ]
 
-
 export default function Header() {
     return (
         <div className="w-full min-h-[30px] p-4 shadow-lg">
             <div className="flex">
                 {ROUTES.map(route => {
+                    if (route.path === '/') {
+                        return <a href={route.path} key={route.path}
+                            className="p-3 text-lg underline underline-offset-4 decoration-2 transition hover:text-orange-400 duration-500">
+                            {route.title}
+                        </a>
+
+                    }
                     return <Link href={route.path} key={route.path}
                         className="p-3 text-lg underline underline-offset-4 decoration-2 transition hover:text-orange-400 duration-500">
                         {route.title}
