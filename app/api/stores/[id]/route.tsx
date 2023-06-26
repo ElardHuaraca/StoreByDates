9dcf5695256700fff458b01120798afc132f927c
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-26 14:46:05 -0500

    Update route now use params

--- app/api/stores/[id]/route.tsx
+++ app/api/stores/[id]/route.tsx
@@ -1,10 +1,10 @@
 import { StoreById } from "@/utils/DatabaseController";
 import { NextApiRequest } from "next";
+import { NextResponse } from "next/server";
 
-export async function GET(request: NextApiRequest) {
-    const { id } = request.query;
-
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const { id } = params;
     const store = await StoreById(Number(id))
 
-    return store
+    return NextResponse.json(store);
 }
