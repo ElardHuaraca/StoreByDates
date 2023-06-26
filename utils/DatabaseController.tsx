4ab079bdabd31abf0e175d6821311364fd74f078
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-26 13:30:06 -0500

    Update table name

--- utils/DatabaseController.tsx
+++ utils/DatabaseController.tsx
@@ -13,7 +13,7 @@ const connect = connection.promise()
 
 export async function AllStores() {
 
-    const [results, _] = await connect.execute('SELECT id, name FROM stores')
+    const [results, _] = await connect.execute('SELECT id, name FROM store')
 
     return results
 }
