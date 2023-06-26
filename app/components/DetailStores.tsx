26424ff6a8b3e9843a06afc0c4e4c24d4c4d36cd
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-26 13:10:29 -0500

    Implement DetailsStores component

--- app/components/DetailStores.tsx
+++ app/components/DetailStores.tsx
@@ -1,5 +1,22 @@
 'use client'
 
-export async function DetailStores({ id }: { id: string }) {
+import { useState } from "react"
+import { fetchStoresById } from "./FetchStoreCatalyst"
+
+export function DetailStores({ id }: { id: string }) {
+    const [fetchState, setFetchState] = useState<string | null>(null)
+    const [isLoading, setLoading] = useState(true)
+
+    const fetchDetails = async () => {
+        const result = await fetchStoresById(id)
+        setFetchState(result)
+        setLoading(false)
+    }
+    return <details onClick={() => { fetchDetails() }}>
+        <summary className="cursor-pointer">
+            Mostrar Stores
+        </summary>
+        {isLoading ? <div>Loading . . .</div> : <p>{fetchState}</p>}
+    </details>
 
 }
