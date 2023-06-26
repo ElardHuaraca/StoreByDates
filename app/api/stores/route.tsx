a2033ce54d6af1971bfd0813a1a0b6fc0185e023
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-25 23:33:43 -0500

    Implement post to route

--- app/api/stores/route.tsx
+++ app/api/stores/route.tsx
@@ -1,10 +1,13 @@
 import { AllStores } from "@/utils/DatabaseController";
 import { NextResponse } from "next/server";
 
-
 export async function GET() {
-
+    /* avoid cache */
     const stores = await AllStores()
 
     return NextResponse.json(stores)
+}
+
+export async function POST(request: Request) {
+    return NextResponse.json(await request.json())
 }
