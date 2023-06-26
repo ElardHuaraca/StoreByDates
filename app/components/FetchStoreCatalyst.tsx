014e753087dfe00f8050abaad6652d33c6da4251
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-26 14:16:04 -0500

    Add component fetch Storebyid all catalyst

--- app/components/FetchStoreCatalyst.tsx
+++ app/components/FetchStoreCatalyst.tsx
@@ -1,6 +1,22 @@
 'use server'
+const STRUCTURES = [
+    {
+        types: '3640, 4640, 5650',
+        api_auth: '/pml/login/authenticatewithobject'
+    },
+    {
+        types: '4900, 6600',
+        api_auth: '/fusion/authheartbeat?media=txt'
+    }
+]
 
+/* https://10.0.75.57/pml/login/authenticatewithobject
+ */
 export async function fetchStoresById(id: string) {
+
+    const api = () => {
+
+    }
     return new Promise<string>(async (resolve, _) => {
         setTimeout(() => resolve('response'), 5000)
     })
