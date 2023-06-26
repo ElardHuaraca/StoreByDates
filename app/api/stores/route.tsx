4887fd5d99a7afdbc3d3f7506b9a0933c352054a
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-26 11:33:17 -0500

    Update responde if stores is empty

--- app/api/stores/route.tsx
+++ app/api/stores/route.tsx
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     /* avoid cache */
-    const stores = await AllStores()
+    const stores = await AllStores() as Store[]
+
+    if (stores.length === 0) return NextResponse.json(null)
 
     return NextResponse.json(stores)
 }
