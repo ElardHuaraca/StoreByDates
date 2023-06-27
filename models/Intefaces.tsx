67343bd5d77ef3da323d583a648c41ebd4c42dfb
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-27 15:44:23 -0500

    Rename to model or entity, implement two entity

--- models/Intefaces.tsx
+++ models/Intefaces.tsx
@@ -1,9 +1,21 @@
-interface Store {
+interface StoreModel {
     id: number
     name: string
 }
 
-interface DetailStore {
+interface StoreEntity {
+    id: number
+    name: string
+    ip: string
+    type_id: number
+}
+
+interface Type_Store{
+    id: number
+    name: string
+}
+
+interface DetailStoreModel {
     ip: string,
     type_name: string
 }
