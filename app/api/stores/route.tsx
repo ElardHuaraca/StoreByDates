6c8e557e9cf69be9006638d485e6fe74b6beb248
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-28 03:23:25 -0500

    Add init first time database

--- app/api/stores/route.tsx
+++ app/api/stores/route.tsx
@@ -1,13 +1,20 @@
-import { AllStores } from "@/helpers/DatabaseController";
+import { AllStores, InitDatabaseAndModels } from "@/helpers/DatabaseController";
 import { NextResponse } from "next/server";
 
+let Initialized = false
+
 export async function GET() {
+    if (!Initialized) {
+        await InitDatabaseAndModels()
+        Initialized = true
+    }
+
     /* avoid cache */
     const stores = await AllStores() as IStoreModel[]
 
     if (stores.length === 0) return NextResponse.json({}, { status: 404 })
 
-    return NextResponse.json(stores)
+    return NextResponse.json([])
 }
 
 export async function POST(request: Request) {
