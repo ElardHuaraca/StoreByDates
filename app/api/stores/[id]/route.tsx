7252b9347cd3ecd3a7ccb2a25b911c95046a1938
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-28 22:08:20 -0500

    Update process result on database

--- app/api/stores/[id]/route.tsx
+++ app/api/stores/[id]/route.tsx
@@ -2,11 +2,11 @@ import { StoreById } from "@/helpers/DatabaseController";
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(_: Request, { params }: { params: { id: string } }) {
     const { id } = params;
-    const store = await StoreById(Number(id)) as DetailStore[]
+    const store = await StoreById(id) as unknown as IDetailStoreModel[]
 
-    if (store.length === 0) return NextResponse.json({}, { status: 404 })
+    if (store?.length === 0 || store === null) return NextResponse.json({}, { status: 404 })
 
     return NextResponse.json(store[0]);
 }
