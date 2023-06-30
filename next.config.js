7d0e620976a0d95864ffde87efba6eec3dff523b
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-30 02:41:51 -0500

    Add copy file to public folder

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
+const CopyWebPackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental:
     {
         serverActions: true,
         serverComponentsExternalPackages: ['sequelize']
+    },
+    webpack: (config, { isServer }) => {
+        config.plugins.push(
+            new CopyWebPackPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'node_modules/flowbite/dist/flowbite.min.js'),
+                        to: path.resolve(__dirname, 'public/js')
+                    }
+                ]
+            })
+        )
+
+        return config
     }
 }
 
