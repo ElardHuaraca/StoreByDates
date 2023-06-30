4ba7877e7e51423f0efb07eeef1e7429938fb560
ElardHuaraca <elard.sd.9@gmail.com>
2023-06-30 02:42:54 -0500

    Load Script flowbite on main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} min-h-screen`}>
         <Header />
         {children}
+        <Script src="/js/flowbite.min.js" strategy="beforeInteractive"></Script>
       </body>
     </html>
   )
