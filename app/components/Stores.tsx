d335d0bc28793feeb9cc5f4a620fca99de5a6858
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-02 00:57:30 -0500

    Update return

--- app/components/Stores.tsx
+++ app/components/Stores.tsx
@@ -11,30 +11,34 @@ const fetchStores = () => fetch(process.env.API_ROUTE_BASE + "/stores", {
 type TStoreType = 'show' | 'write'
 
 const STORES_TYPE_SHOW = ({ stores }: { stores: IStoreModel[] }) => {
-    return <table className="table-auto border-separate w-full text-center border-spacing-0">
-        <thead>
-            <tr>
-                <th className=" w-1/2 border-l-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tl-2xl">STORE ONCE</th>
-                <th className=" w-1/2 border-[3px] border-emerald-400 bg-emerald-600 rounded-tr-2xl">STORES</th>
-            </tr>
-        </thead>
-        <tbody>
-            {stores?.map((store) => {
-                return <tr key={store.id}>
-                    <td className="border-b-2 border-l-2 border-emerald-400 py-2">{store.name}</td>
-                    <td className="border-b-2 border-x-2 border-emerald-400 py-2">
-                        <DetailStores id={`${store.id}`} />
-                    </td>
+    return (
+        <table className="table-auto border-separate w-full text-center border-spacing-0">
+            <thead>
+                <tr>
+                    <th className=" w-1/2 border-l-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tl-2xl">STORE ONCE</th>
+                    <th className=" w-1/2 border-[3px] border-emerald-400 bg-emerald-600 rounded-tr-2xl">STORES</th>
                 </tr>
-            }) || <tr><td colSpan={3} className="border-b-2 border-x-2 border-emerald-400 py-2">No se ha registrado ningun STORE ONCE</td></tr>
-            }
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                {stores?.map((store) => {
+                    return (
+                        <tr key={store.id}>
+                            <td className="border-b-2 border-l-2 border-emerald-400 py-2">{store.name}</td>
+                            <td className="border-b-2 border-x-2 border-emerald-400 py-2">
+                                <DetailStores id={`${store.id}`} />
+                            </td>
+                        </tr>
+                    )
+                }) || <tr><td colSpan={3} className="border-b-2 border-x-2 border-emerald-400 py-2">No se ha registrado ningun STORE ONCE</td></tr>
+                }
+            </tbody>
+        </table>
+    )
 }
 
 const STORE_TYPE_WRITE = async ({ stores }: { stores: IStoreModel[] }) => {
 
-    return <table className="table-auto border-separate w-full text-center border-spacing-0">
+    return (<table className="table-auto border-separate w-full text-center border-spacing-0">
         <thead>
             <tr>
                 <th className=" w-1/3 border-l-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tl-2xl">STORE ONCE</th>
@@ -44,17 +48,20 @@ const STORE_TYPE_WRITE = async ({ stores }: { stores: IStoreModel[] }) => {
         </thead>
         <tbody>
             {stores?.map((store) => {
-                return <tr key={store.id}>
-                    <td className="border-b-2 border-l-2 border-emerald-400 py-2">{store.name}</td>
-                    <td className="border-b-2 border-x-2 border-emerald-400 py-2">{store.ip}</td>
-                    <td className="border-b-2 border-x-2 border-emerald-400 py-2">
-                        <CreateUpdateStore id={`${store.id}`} />
-                    </td>
-                </tr>
+                return (
+                    <tr key={store.id}>
+                        <td className="border-b-2 border-l-2 border-emerald-400 py-2">{store.name}</td>
+                        <td className="border-b-2 border-x-2 border-emerald-400 py-2">{store.ip}</td>
+                        <td className="border-b-2 border-x-2 border-emerald-400 py-2">
+                            <CreateUpdateStore id={`${store.id}`} />
+                        </td>
+                    </tr>
+                )
             }) || <tr><td colSpan={3} className="border-b-2 border-x-2 border-emerald-400 py-2">No se ha registrado ningun STORE ONCE</td></tr>
             }
         </tbody>
     </table>
+    )
 }
 
 export default async function Stores({ type }: { type: TStoreType }) {
