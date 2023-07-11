757796f3a1a0ebc4d339e9a6051291e7aa1b2af5
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-11 16:41:54 -0500

    Add store suppot helper

---
+++ helpers/StoreSuport.tsx
@@ -0,0 +1,10 @@
+export const STRUCTURES = [
+    {
+        types: '3640, 4640, 5650',
+        api_auth: '/pml/login/authenticatewithobject'
+    },
+    {
+        types: '4900, 6600',
+        api_auth: '/fusion/authheartbeat?media=txt'
+    }
+]
