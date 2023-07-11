88a80c69cc84adde81d4c00d9417982a08e7bc92
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-11 15:09:05 -0500

    Update button to modal

--- app/components/UDStoreComponent.tsx
+++ app/components/UDStoreComponent.tsx
@@ -1,4 +1,5 @@
 import { ButtonComponent } from "./ButtonComponent"
+import CRUDModal from "./CRUDModal"
 import TrashIcon from "./svg/TrashIcon"
 import WriteIcon from "./svg/WriteIcon"
 
@@ -7,13 +8,11 @@ export default function UDStoreComponent({ id }: { id: string }) {
         <div className="flex flex-row justify-center">
             <div className="p-2">
                 <ButtonComponent textButton={<TrashIcon className="w-full" />}
-                    className="bg-red-500 rounded-lg py-2 px-4 w-14 h-10 flex items-center content-center transition hover:bg-red-400 duration-300"
-                    type="button" />
+                    className="bg-red-500 rounded-lg py-2 px-4 w-14 h-10 flex items-center content-center transition hover:bg-red-400 duration-300" />
             </div>
             <div className="p-2">
-                <ButtonComponent textButton={<WriteIcon className="w-11/12" />}
-                    data-modal-target="modal_" data-modal-toggle="modal_" id="download2"
-                    className="bg-sky-500 rounded-lg py-2 px-4 w-14 h-10 flex items-center content-center transition hover:bg-sky-400 duration-300" />
+                <CRUDModal id={id} textButton={<WriteIcon className="w-11/12" />}
+                    styleButton="bg-sky-500 rounded-lg py-2 px-4 w-14 h-10 flex items-center content-center transition hover:bg-sky-400 duration-300" />
             </div>
         </div>
     )
