b7c9cf03a2ced8c75406ff5c372ad8bc4a6f77a7
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-17 18:21:00 -0500

    Add delete store by id

--- app/api/stores/[id]/route.tsx
+++ app/api/stores/[id]/route.tsx
@@ -1,4 +1,4 @@
-import { StoreById } from "@/helpers/DatabaseController";
+import { DeleteStore, StoreById } from "@/helpers/DatabaseController";
 import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
@@ -9,4 +9,11 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
     if (store === null) return NextResponse.json({}, { status: 404 })
 
     return NextResponse.json(store);
+}
+
+export async function DELETE(_: Request, { params }: { params: { id: string } }) {
+    const { id } = params
+    const response = await DeleteStore(id)
+    if (response !== 1) return NextResponse.json({ status: 404 })
+    return NextResponse.json({ status: 200 })
 }
