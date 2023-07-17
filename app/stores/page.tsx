43a08be19984311657ff0f9d00e881137175c703
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-17 18:20:35 -0500

    Update button to modal for create store

--- app/stores/page.tsx
+++ app/stores/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next"
-import { ButtonComponent } from "../components/ButtonComponent"
 import Stores from "../components/Stores"
+import CRUDModal from "../components/CRUDModal"
 
 export const metadata: Metadata = {
     title: 'Manejo de Stores'
@@ -11,8 +11,7 @@ export default function StoreHome() {
     return (
         <main className="p-8">
             <div className="w-1/6 flex justify-center border-2 border-white rounded-lg p-3">
-                <ButtonComponent textButton="Añadir Store" data-modal-target="modal_" data-modal-toggle="modal_"
-                    className="p-2 bg-sky-400 text-black rounded-lg font-medium transition hover:bg-sky-600  duration-200" />
+                <CRUDModal textButton="Añadir Store" styleButton="p-2 bg-sky-400 text-black rounded-lg font-medium transition hover:bg-sky-600  duration-200" />
             </div>
             <div className="min-w-full py-4">
                 <Stores type="write" />
