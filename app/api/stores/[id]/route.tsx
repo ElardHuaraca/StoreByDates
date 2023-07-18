f848191ba82fe729878a3521664748debf8f02ea
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-17 23:24:58 -0500

    Add update store by id

--- app/api/stores/[id]/route.tsx
+++ app/api/stores/[id]/route.tsx
@@ -1,6 +1,5 @@
-import { DeleteStore, StoreById } from "@/helpers/DatabaseController";
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import { DeleteStore, StoreById, UpdateStore } from "@/helpers/DatabaseController";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
     const { id } = params;
@@ -14,6 +13,20 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
 export async function DELETE(_: Request, { params }: { params: { id: string } }) {
     const { id } = params
     const response = await DeleteStore(id)
-    if (response !== 1) return NextResponse.json({ status: 404 })
+    if (response !== 1) return NextResponse.json({}, { status: 404 })
     return NextResponse.json({ status: 200 })
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = params
+    const { name_store, ip_store, version_store } = Object.fromEntries(await request.formData())
+    const type = version_store === 'na' ? undefined : Number(version_store)
+    const response = await UpdateStore({ id, name: name_store.toString(), ip: ip_store.toString(), type })
+
+    if (response !== 1) return NextResponse.json({}, { status: 404 })
+
+    const store = await StoreById(id)
+    console.log(store)
+
+    return NextResponse.json(store)
 }
