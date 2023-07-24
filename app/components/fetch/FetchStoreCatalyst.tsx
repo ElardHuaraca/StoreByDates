72caf5b86ba3fae0a9bce218a901abe6ffc968ed
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-24 15:33:10 -0500

    Add suport fetch store < 5650

--- app/components/fetch/FetchStoreCatalyst.tsx
+++ app/components/fetch/FetchStoreCatalyst.tsx
@@ -1,27 +1,88 @@
 'use server'
+import { STRUCTURES, StoreType, Structure } from '@/helpers/StoreSuport'
 
-const STRUCTURES = [
-    {
-        types: '3640, 4640, 5650',
-        api_auth: '/pml/login/authenticatewithobject'
-    },
-    {
-        types: '4900, 6600',
-        api_auth: '/fusion/authheartbeat?media=txt'
-    }
-]
-
-const fetchStoreDetails = async (id: string) => {
-    const response = await fetch(process.env.API_ROUTE_BASE + `/stores/${id}`)
-    return response.json()
+/* type_function_by_store_type by StoreType return await functions */
+const type_function_by_store_type = {
+    [StoreType.TYPE_1]: fetchStoresLower5650,
+    [StoreType.TYPE_2]: fetchStoresLower5650,
 }
 
-/* https://10.0.75.57/pml/login/authenticatewithobject
- */
-export async function fetchStoresCatalystById(id: string) {
-    const details = await fetchStoreDetails(id) as IDetailStoreModel
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-    if (Object.keys(details).length === 0) return null
+export async function fetchStoresCatDalyst({ data }: { data: IStoreModel }) {
+    const data_Type = data.type_store?.name ?? false
+    if (!data_Type) return <p className='text-lg'>Por favor asigne un tipo de store</p>
 
-    return details
+    const type = STRUCTURES.find((item) => item.types.includes(data_Type)) ?? false
+    if (!type) return <p className='text-lg'>La version brindada no tiene soporte</p>
+
+    /* call type_function_by_store_type */
+    const response = await type_function_by_store_type[StoreType.TYPE_1]({ type, data })
+
+
+    return (
+        <ul>
+            {response}
+        </ul>
+    )
+}
+
+async function fetchStoresLower5650({ type, data }: { type: Structure, data: IStoreModel }) {
+    const requestAuth: RequestInit = {
+        cache: 'no-cache',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            'username': process.env.USER_STORE,
+            'password': process.env.PASSWORD_STORE,
+            'grant_type': 'password'
+        }),
+
+    }
+
+    const credentials = await fetch(`https://${data.ip}/${type.api_auth}`, requestAuth)
+        .then(res => {
+            if (res.ok) return res.json()
+            else return <p className='text-lg'>Error al autenticarse</p>
+        }).catch(e => console.log(e))
+
+    if (credentials.access_token === undefined) return credentials
+
+    const requestUUID: RequestInit = {
+        cache: 'no-cache',
+        headers: {
+            'Authorization': type.authorization(credentials.access_token),
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const credentials_uuid = await fetch(`https://${data.ip}/${type.api_uuid}`, requestUUID)
+        .then(async (res) => {
+            if (res.ok) {
+                const xml = await res.text()
+                return xml.slice(xml.indexOf("<uuid>") + 6, xml.indexOf("</uuid>"))
+            }
+            else return <p className='text-lg'>Error al obtener la identidad del Store</p>
+        }).catch(e => console.log(e))
+
+    if (typeof credentials_uuid !== 'string') return credentials_uuid
+
+    const store_catalyst = await fetch(`https://${data.ip}/${type.api_catalyst}?category=catalyst-stores&count=20&start=0&query=(systemUuid%3A%27${credentials_uuid}%27)%20AND%20(isCloudStore%3A%27false%27)&sort=name%3Aasc`, requestUUID)
+        .then(res => {
+            if (res.ok) return res.json()
+            else return <p className='text-lg'>Error al obtener los Almacenes Catalysts</p>
+        }).catch(e => console.log(e))
+
+    return (
+        <>
+            {store_catalyst.members.map((item: any) => {
+                return (
+                    <li key={item.systemUuid}>
+                        <p>{item.name}</p>
+                    </li>
+                )
+            })}
+        </>
+    )
+
 }
