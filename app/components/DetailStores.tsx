e6d82838a176fb26b9de642756650cfcee5c5190
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-24 15:16:05 -0500

    Update message for show info stores

--- app/components/DetailStores.tsx
+++ app/components/DetailStores.tsx
@@ -1,23 +1,23 @@
 'use client'
 
 import { useState } from "react"
-import { fetchStoresCatalystById } from "./fetch/FetchStoreCatalyst"
+import { fetchStoresCatDalyst } from "./fetch/FetchStoreCatalyst"
 
-export default function DetailStores({ id }: { id: string }) {
-    const [fetchState, setFetchState] = useState<IDetailStoreModel | null>(null)
+export default function DetailStores({ store }: { store: IStoreModel }) {
+    const [fetchState, setFetchState] = useState<any>(null)
     const [isLoading, setLoading] = useState(true)
 
     const fetchDetails = async () => {
         if (!isLoading) return
-        const result = await fetchStoresCatalystById(id)
+        const result = await fetchStoresCatDalyst({ data: store })
         setFetchState(result)
         setLoading(false)
     }
     return <details onClick={() => { fetchDetails() }}>
         <summary className="cursor-pointer">
-            Mostrar Stores
+            Mostrar Almacenes Catalyst
         </summary>
-        {isLoading ? <div>Loading . . .</div> : <p>{fetchState?.ip}</p>}
+        {isLoading ? <div className="p-4">Loading . . .</div> : <div className="p-4">{fetchState}</div>}
     </details>
 
 }
