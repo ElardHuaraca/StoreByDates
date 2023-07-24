55e7a0474030286b0e22bcdabadd77eab7b1bea9
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-24 15:14:19 -0500

    Update suport structure to stores

--- helpers/StoreSuport.tsx
+++ helpers/StoreSuport.tsx
@@ -1,10 +1,30 @@
-export const STRUCTURES = [
+/* types */
+export type Structure = {
+    types: string,
+    api_auth: string,
+    api_uuid?: string,
+    api_catalyst?: string,
+    authorization: (key: string) => string
+
+}
+
+/* enum */
+export enum StoreType {
+    TYPE_1, TYPE_2
+}
+
+/* data */
+export const STRUCTURES: Structure[] = [
     {
         types: '3640, 4640, 5650',
-        api_auth: '/pml/login/authenticatewithobject'
+        api_auth: 'pml/login/authenticatewithobject',
+        api_uuid: 'pml/clustermanagement',
+        api_catalyst: 'rest/index/resources',
+        authorization: (key: string) => { return `Bearer ${key}` }
     },
     {
         types: '4900, 6600',
-        api_auth: '/fusion/authheartbeat?media=txt'
+        api_auth: '/fusion/authheartbeat?media=txt',
+        authorization: (key: string) => { return `Basic ${btoa(key)}` }
     }
 ]
