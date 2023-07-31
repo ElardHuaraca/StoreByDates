237d1cd092f68870be14cd331fdd6dc7ab4b24f9
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-31 11:13:40 -0500

    Add support store 4900,6600

--- app/components/fetch/FetchStoreCatalyst.tsx
+++ app/components/fetch/FetchStoreCatalyst.tsx
@@ -13,11 +13,11 @@ export async function fetchStoresCatDalyst({ data }: { data: IStoreModel }) {
     const data_Type = data.type_store?.name ?? false
     if (!data_Type) return <p className='text-lg'>Por favor asigne un tipo de store</p>
 
-    const type = STRUCTURES.find((item) => item.types.includes(data_Type)) ?? false
-    if (!type) return <p className='text-lg'>La version brindada no tiene soporte</p>
+    const store_type = STRUCTURES.find((item) => item.types.includes(data_Type)) ?? false
+    if (!store_type) return <p className='text-lg'>La version brindada no tiene soporte</p>
 
     /* call type_function_by_store_type */
-    const response = await type_function_by_store_type[StoreType.TYPE_1]({ type, data })
+    const response = await type_function_by_store_type[store_type.type]({ type: store_type, data })
 
     return (
         <ul>
@@ -43,7 +43,10 @@ async function fetchStoresLower5650({ type, data }: { type: Structure, data: ISt
         .then(res => {
             if (res.ok) return res.json()
             else return <p className='text-lg'>Error al autenticarse</p>
-        }).catch(e => console.log(e))
+        }).catch(e => {
+            console.log(e)
+            return <p className='text-lg'>Ocurrio un error inesperado</p>
+        })
 
     if (credentials === undefined || credentials.access_token === undefined) return credentials
 
@@ -62,7 +65,10 @@ async function fetchStoresLower5650({ type, data }: { type: Structure, data: ISt
                 return xml.slice(xml.indexOf("<uuid>") + 6, xml.indexOf("</uuid>"))
             }
             else return <p className='text-lg'>Error al obtener la identidad del Store</p>
-        }).catch(e => console.log(e))
+        }).catch(e => {
+            console.log(e)
+            return <p className='text-lg'>Ocurrio un error inesperado</p>
+        })
 
     if (typeof credentials_uuid !== 'string') return credentials_uuid
 
@@ -70,7 +76,10 @@ async function fetchStoresLower5650({ type, data }: { type: Structure, data: ISt
         .then(res => {
             if (res.ok) return res.json()
             else return <p className='text-lg'>Error al obtener los Almacenes Catalysts</p>
-        }).catch(e => console.log(e))
+        }).catch(e => {
+            console.log(e)
+            return <p className='text-lg'>Ocurrio un error inesperado</p>
+        })
     return (
         <>
             {store_catalyst.members.map((item: any) => {
@@ -101,5 +110,48 @@ async function fetchStoresLower5650({ type, data }: { type: Structure, data: ISt
 }
 
 async function fetStores49006600({ type, data }: { type: Structure, data: IStoreModel }) {
+    const authorization = type.authorization(`${process.env.USER_STORE}:${process.env.PASSWORD_STORE}`)
+    const requestInit: RequestInit = {
+        headers: {
+            'Authorization': authorization,
+        }
+    }
 
+    const store_catalyst = await fetch(`https://${data.ip}/${type.api_catalyst}`, requestInit)
+        .then(res => {
+            if (res.ok) return res.json()
+            else return <p className='text-lg'>Error al obtener los Almacenes Catalysts</p>
+        }).catch(e => {
+            console.log(e)
+            return <p className='text-lg'>Ocurrio un error inesperado</p>
+        })
+
+    if (store_catalyst === undefined || store_catalyst.list === undefined) return store_catalyst
+
+    return (
+        <>
+            {store_catalyst.list.map((item: any) => {
+                return (
+                    <li key={item.name}>
+                        <div className="flex flex-row ps-4">
+                            <div className="w-1/5 border">
+                                <label htmlFor={`${data.ip}-${item.name}`} className="h-full w-full flex justify-evenly p-2">
+                                    <input type="checkbox" name={`${data.ip}-${item.name}`} id={`${data.ip}-${item.name}`} className="scale-125" />
+                                </label>
+                            </div>
+                            <div className="w-1/2 text-start border">
+                                <p className="p-2">{item.name}</p>
+                            </div>
+                            <div className="w-1/5 border">
+                                <p className="p-2">{StoreConvertByteToRespectiveValue({ bytes: item.userBytes })}</p>
+                            </div>
+                            <div className="w-1/5 border">
+                                <p className="p-2">{StoreConvertByteToRespectiveValue({ bytes: item.diskBytes })}</p>
+                            </div>
+                        </div>
+                    </li>
+                )
+            })}
+        </>
+    )
 }
