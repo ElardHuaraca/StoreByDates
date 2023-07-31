36b4479997591ab23be3359943b34e04e00719da
ElardHuaraca <elard.sd.9@gmail.com>
2023-07-31 14:49:57 -0500

    Update component button of generate report

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { ButtonComponent } from './components/ButtonComponent'
+import ButtonGenerateReport from './components/ButtonGenerateReportComponent'
 import Stores from './components/Stores'
 
 export default function Home() {
   return (
     <main className="p-8">
       <div className="w-full flex justify-between border-2 border-white rounded-lg p-3">
-        <ButtonComponent textButton="Generar reporte"
-          className="w-auto p-2 bg-sky-400 text-black rounded-lg font-medium transition hover:bg-sky-600  duration-200" />
+        <ButtonGenerateReport />
         <div className="w-[55%]">
 
         </div>
