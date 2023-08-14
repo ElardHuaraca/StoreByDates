8716065f2a556d6aa144b86714d939f91cba1882
ElardHuaraca <elard.sd.9@gmail.com>
2023-08-14 15:05:21 -0500

    remove flex and update header table

--- app/components/Stores.tsx
+++ app/components/Stores.tsx
@@ -23,7 +23,7 @@ const STORES_TYPE_SHOW = ({ stores }: { stores?: IStoreModel[] }) => {
                             <td className="border-b-2 border-x-2 border-emerald-400 py-2">
                                 <DetailStores store={store} />
                             </td>
-                            <td className="border-b-2 border-e-2 border-emerald-400 py-2 flex gap-2 px-3 items-center justify-center relative">
+                            <td className="border-b-2 border-e-2 border-emerald-400 py-2 gap-2 px-3 ">
                                 <DateTimePicker name_input="date_start" content_span="Inicio: " />
                                 <DateTimePicker name_input="date_end" content_span="Fin: " />
                             </td>
@@ -38,31 +38,32 @@ const STORES_TYPE_SHOW = ({ stores }: { stores?: IStoreModel[] }) => {
 
 const STORE_TYPE_WRITE = async ({ stores }: { stores?: IStoreModel[] }) => {
 
-    return (<table className="table-auto border-separate w-full text-center border-spacing-0">
-        <thead>
-            <tr>
-                <th className=" w-1/4 border-l-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tl-2xl">STORE ONCE</th>
-                <th className=" w-1/4 border-[3px] border-emerald-400 bg-emerald-600">IP</th>
-                <th className=" w-1/4 border-[3px] border-emerald-400 bg-emerald-600">Modelo</th>
-                <th className=" w-1/4 border-r-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tr-2xl">Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            {stores?.map((store) => {
-                return (
-                    <tr key={store.id} tr-key={store.id}>
-                        <td className="border-b-2 border-l-2 border-emerald-400 py-2">{store.name}</td>
-                        <td className="border-b-2 border-x-2 border-emerald-400 py-2">{store.ip}</td>
-                        <td className="border-b-2 border-r-2 border-emerald-400 py-2">{store.type_store === null ? 'N.A.' : store.type_store?.name}</td>
-                        <td className="border-b-2 border-x-2 border-emerald-400 py-2">
-                            <UDStoreComponent data={store} />
-                        </td>
-                    </tr>
-                )
-            }) || <tr><td colSpan={4} className="border-b-2 border-x-2 border-emerald-400 py-2">No se ha registrado ningun STORE ONCE</td></tr>
-            }
-        </tbody>
-    </table>
+    return (
+        <table className="table-auto border-separate w-full text-center border-spacing-0">
+            <thead>
+                <tr>
+                    <th scope="col" className=" w-1/4 border-l-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tl-2xl">STORE ONCE</th>
+                    <th scope="col" className=" w-1/4 border-[3px] border-emerald-400 bg-emerald-600">IP</th>
+                    <th scope="col" className=" w-1/4 border-[3px] border-emerald-400 bg-emerald-600">Modelo</th>
+                    <th scope="col" className=" w-1/4 border-r-[3px] border-y-[3px] border-emerald-400 bg-emerald-600 rounded-tr-2xl">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {stores?.map((store) => {
+                    return (
+                        <tr key={store.id} tr-key={store.id}>
+                            <td className="border-b-2 border-l-2 border-emerald-400 py-2">{store.name}</td>
+                            <td className="border-b-2 border-x-2 border-emerald-400 py-2">{store.ip}</td>
+                            <td className="border-b-2 border-r-2 border-emerald-400 py-2">{store.type_store === null ? 'N.A.' : store.type_store?.name}</td>
+                            <td className="border-b-2 border-x-2 border-emerald-400 py-2">
+                                <UDStoreComponent data={store} />
+                            </td>
+                        </tr>
+                    )
+                }) || <tr><td colSpan={4} className="border-b-2 border-x-2 border-emerald-400 py-2">No se ha registrado ningun STORE ONCE</td></tr>
+                }
+            </tbody>
+        </table>
     )
 }
 
