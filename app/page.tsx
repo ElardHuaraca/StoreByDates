f0cc5d838cb4e8b40a035d2b6844827c44bff46f
ElardHuaraca <elard.sd.9@gmail.com>
2023-08-14 15:04:50 -0500

    Update with relacion screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { ButtonComponent } from './components/ButtonComponent'
 import ButtonGenerateReport from './components/ButtonGenerateReportComponent'
 import DateTimePicker from './components/DateTimePicker'
-import LoadingBar from './components/LoadingBar'
 import Stores from './components/Stores'
 
 export default function Home() {
@@ -9,20 +8,20 @@ export default function Home() {
     <main className="p-8">
       <div className="w-full flex justify-between border-2 border-white rounded-lg p-3">
         <ButtonGenerateReport />
-        <div className="w-[65%] flex items-center">
-          <div className="flex w-1/5 relative items-center gap-2">
-            <DateTimePicker name_input="date_start" content_span="Inicio: " />
-            <DateTimePicker name_input="date_end" content_span="Fin: " />
-          </div>
-          <div className="min-w-[80%]">
-            <LoadingBar id="loading_bar" />
+        <div className="w-[65%] flex px-4">
+          <div className="w-full flex items-center gap-4">
+            <div className="flex items-center gap-1">
+              <DateTimePicker name_input="date_start" content_span="Inicio: " />
+              <DateTimePicker name_input="date_end" content_span="Fin: " />
+            </div>
+            <div className="w-5/6" id="loading_container"></div>
           </div>
         </div>
         <ButtonComponent textButton="Descargar reporte"
           className="w-auto p-2 bg-teal-500 text-black rounded-lg font-medium transition hover:bg-teal-700  duration-200" />
       </div>
       <div className="min-w-full py-4">
-        <div className="border-2 border-amber-200 rounded-lg min-h-[80px] p-4 overflow-hidden">
+        <div className="border-2 border-amber-200 rounded-lg min-h-[80px] p-4 overflow-auto">
           <Stores type='show' />
         </div>
       </div>
