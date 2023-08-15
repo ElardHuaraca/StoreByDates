9bf9f6c10667d8192481507d05fcace973bef296
ElardHuaraca <elard.sd.9@gmail.com>
2023-08-15 15:40:25 -0500

    Add function to support cookie store 4900,6600

--- models/Types.tsx
+++ models/Types.tsx
@@ -7,6 +7,7 @@ export type Structure = {
     api_uuid?: string,
     api_catalyst?: string,
     api_elements_all: ({ key_1, key_2 }: { key_1: string, key_2?: string }) => string,
+    cokie_to_dates?: ({ time_start, time_end }: { time_start: string, time_end: string }) => string,
     authorization: (key: string) => string
 
 }
