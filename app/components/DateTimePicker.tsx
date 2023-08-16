6ee70c213805b33c40c68605bf6444c444120a96
ElardHuaraca <elard.sd.9@gmail.com>
2023-08-16 14:43:41 -0500

    Update datepicker component to relative container

--- app/components/DateTimePicker.tsx
+++ app/components/DateTimePicker.tsx
@@ -8,12 +8,12 @@ export default function DateTimePicker({ name_input, content_span }: { name_inpu
     const handleIconClick = () => { inputRef.current?.showPicker() }
 
     return (
-        <>
+        <div className="relative">
             <span>{content_span}</span>
             <label className="inline-block w-8 h-8 cursor-pointer text-2xl" onClick={handleIconClick} htmlFor={name_input}>
                 📅
             </label>
             <input type="datetime-local" name={name_input} ref={inputRef} className="absolute left-0 bottom-0 w-0 h-0 cursor-pointer" />
-        </>
+        </div>
     )
 }
